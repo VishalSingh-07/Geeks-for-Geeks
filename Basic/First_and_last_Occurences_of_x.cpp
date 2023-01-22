b335ccb5387718f95473228579acb3d4088990b2
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-01-22 17:29:27 +0530

    Adding first and last occurrences of x optimized approach

--- Basic/First_and_last_Occurences_of_x.cpp
+++ Basic/First_and_last_Occurences_of_x.cpp
@@ -3,36 +3,93 @@
 using namespace std;
 
 // } Driver Code Ends
+
+// Brute Force Approach
+// Time complexity --> O(n) and space --> O(n)
+// vector<int> find(int arr[], int n , int x )
+// {
+//     // code here
+//     vector<int> v;
+//     for(int i=0;i<n;i++)
+//     {
+//         if(arr[i]==x)
+//         {
+//             v.push_back(i);
+//             break;
+//         }
+        
+//     }
+//     for(int i=n-1;i>0;i--)
+//     {
+//         if(arr[i]==x)
+//         {
+//             v.push_back(i);
+//             break;
+//         }
+        
+//     }
+//     if(v.empty())
+//     {
+//         v.push_back(-1);
+//         v.push_back(-1);
+//     }
+//     return v;
+// }
+
+
+// Optimized Approach 
+// Time complexity --> O(logn) and Space --> O(1)
 vector<int> find(int arr[], int n , int x )
 {
-    // code here
-    vector<int> v;
-    for(int i=0;i<n;i++)
+    int index1=-1;
+    int index2=-1;
+    int low=0;
+    int high=n-1;
+    while(low<=high) // for searching an element on the left side of an array
     {
-        if(arr[i]==x)
+        int mid=low+(high-low)/2;
+        if(arr[mid]==x)
         {
-            v.push_back(i);
-            break;
+            index1=mid;
+            high=mid-1;
         }
-        
-    }
-    for(int i=n-1;i>0;i--)
-    {
-        if(arr[i]==x)
+        else if(arr[mid]>x) // move to left
         {
-            v.push_back(i);
-            break;
+            high=mid-1;
         }
-        
+        else // move to right
+        {
+            low=mid+1;
+        }
     }
-    if(v.empty())
-    {
-        v.push_back(-1);
-        v.push_back(-1);
-    }
-    return v;
     
+    low=0;
+    high=n-1;
+    while(low<=high) // for searching an element on the right side of an array
+    {
+        int mid=low+(high-low)/2;
+        if(arr[mid]==x)
+        {
+            index2=mid;
+            low=mid+1;
+        }
+        else if(arr[mid]>x)
+        {
+            high=mid-1;
+        }
+        else
+        {
+            low=mid+1;
+        }
+    }
+    if(index1==-1 && index2==-1)
+    {
+        return {-1,-1};
+    }
+    return {index1, index2};
 }
+
+
 
 //{ Driver Code Starts.
 
