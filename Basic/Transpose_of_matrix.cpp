5f81d361524119eb68c4f71f074724f9fd48dc6b
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-02-02 16:32:32 +0530

    Adding Transpose of matrix question

---
+++ Basic/Transpose_of_matrix.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+// Time complexity --> O(n*n) and Space --> O(1)
+
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        // code here 
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.transpose(matrix,n);
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// Question link -- https://practice.geeksforgeeks.org/problems/transpose-of-matrix-1587115621/1
