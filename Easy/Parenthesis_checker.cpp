e17547e970dfb95cece013ab680950bae2c98aa3
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-02-03 21:29:33 +0530

    Adding more method for Parenthesis checker question

--- Easy/Parenthesis_checker.cpp
+++ Easy/Parenthesis_checker.cpp
@@ -5,46 +5,101 @@ using namespace std;
 
 // } Driver Code Ends
 
+// Method 1
+// Time complexity --> O(n) and Space --> O(n)
+// class Solution
+// {
+//     public:
+//     //Function to check if brackets are balanced or not.
+//     bool ispar(string x)
+//     {
+//         // Your code here
+//         stack<char> st;
+//         for(int i=0;i<x.length();i++)
+//         {
+//             if(st.empty())
+//             {
+//                 // if the stack is empty just push the current bracket
+//                 st.push(x[i]);
+//             }
+//             else if((st.top()=='(' && x[i]==')') 
+//                         || (st.top()=='{' && x[i]=='}') 
+//                         || (st.top()=='[' && x[i]==']'))
+//             {
+//                 // if we found any complete pair of bracket then pop
+//                 st.pop();
+//             }
+//             else
+//             {
+//                 st.push(x[i]);
+//             }
+            
+//         }
+//         if(st.empty())
+//         {
+//             return true;
+//         }
+//         else
+//         {
+//             return false;
+//         }
+//     }
+
+// };
+
+// Method 2
 // Time complexity --> O(n) and Space --> O(n)
 class Solution
 {
     public:
     //Function to check if brackets are balanced or not.
-    bool ispar(string x)
+    bool ispar(string s)
     {
         // Your code here
         stack<char> st;
-        for(int i=0;i<x.length();i++)
+        for(int i=0;i<s.length();i++)
         {
-            if(st.empty())
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
             {
-                // if the stack is empty just push the current bracket
-                st.push(x[i]);
-            }
-            else if((st.top()=='(' && x[i]==')') 
-                        || (st.top()=='{' && x[i]=='}') 
-                        || (st.top()=='[' && x[i]==']'))
-            {
-                // if we found any complete pair of bracket then pop
-                st.pop();
+                st.push(s[i]);
             }
             else
             {
-                st.push(x[i]);
+                // if a closing bracket then we compare with the top of the stack 
+                // while comparing with top of stack we have 2 cases 
+                // the stack can be empty or the stack is not empty 
+
+                if(st.empty())
+                {
+                    return false;
+                }
+                else
+                {
+                    if((st.top()=='(' && s[i]==')') 
+                        || (st.top()=='[' && s[i]==']') 
+                        || (st.top()=='{' && s[i]=='}'))
+                    {
+                        st.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+
             }
-            
         }
         if(st.empty())
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+        
+        return false;
     }
 
 };
+
 
 //{ Driver Code Starts.
 
