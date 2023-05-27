57f944b23bc2547e1da96c4582196593d04082e5
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-05-27 22:44:22 +0530

    Adding Check if circular linked list  question

---
+++ Basic/Check_if_Circular_Linked_List.cpp
@@ -0,0 +1,122 @@
+/*
+Check If Circular Linked List
+
+
+Given head, the head of a singly linked list, find if the linked list is circular or not. A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. An empty linked list is considered as circular.
+
+Note: The linked list does not contains any inner loop.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+(the first and last node is connected,
+i.e. 5 --> 1)
+Output: 1
+
+
+Example 2:
+
+Input:
+LinkedList: 2->4->6->7->5->1
+Output: 0
+Your Task:
+The task is to complete the function isCircular() which checks if the given linked list is circular or not. It should return true or false accordingly. (the driver code prints 1 if the returned values is true, otherwise 0)
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <=Number of nodes<= 100
+*/
+
+
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+/* Function to get the middle of the linked list*/
+bool isCircular(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+
+        cin>>n>>k;
+	    Node *head=NULL, *tail = NULL;
+	    int x;
+	    cin >> x;
+	    head = new Node(x);
+	    tail = head;
+	    for(int i=0;i<n-1;i++)
+	    {
+	        cin>>x;
+	        tail -> next = new Node(x);
+	        tail = tail -> next;
+	    }
+        if (k==1 && n >= 1)
+              tail->next = head;
+
+
+    printf("%d\n", isCircular(head));
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; 
+/* Should return true if linked list is circular, else false */
+bool isCircular(Node *head)
+{
+  // Your code here
+  Node *p=head;
+  while(p!=NULL)
+  {
+      if(p->next==head)
+      {
+          return true;
+      }
+      p=p->next;
+  }
+  return false;
+}
+
+/*
+Question link -- https://practice.geeksforgeeks.org/problems/circular-linked-list/1
+*/
