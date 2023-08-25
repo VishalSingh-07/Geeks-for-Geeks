e851bbb20179928c0ad81f2a6de0c57c9be4df55
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-08-25 16:04:06 +0530

    Modifying Palindrome String Question

--- Easy/Palindrome_String.cpp
+++ Easy/Palindrome_String.cpp
@@ -6,29 +6,80 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
+
+// Brute Force Approach 
+// Time complexity -> O(n) and Space -> O(n)
+// class Solution{
+// public:	
+	
+	
+// 	int isPalindrome(string S)
+// 	{
+// 	    // Your code goes here
+// 	    string rev="";
+// 	    for(int i=S.size()-1;i>=0;i--)
+// 	    {
+// 	        rev+=S[i];
+// 	    }
+// 	    return S==rev;
+// 	}
+
+// };
+
+
+// Better Approach
+// Time complexity -> O(n/2) and Space -> O(n)
+//User function template for C++
+// class Solution{
+// public:	
+// 	int isPalindrome(string S)
+// 	{
+// 	    // Your code goes here
+// 	    string temp=S;
+// 	    int n=S.length();
+// 	    for(int i=0;i<n/2;i++)
+// 	    {
+// 	        swap(S[i],S[n-i-1]);
+// 	    }
+// 	    if(S==temp)
+// 	    {
+// 	        return 1;
+// 	    }
+// 	    else
+// 	    {
+// 	        return 0;
+// 	    }
+	    
+// 	}
+
+// };
+
+
+// Optimized approach
+// Time complexity -> O(n) and Space -> O(1)
+//User function template for C++
 class Solution{
 public:	
 	int isPalindrome(string S)
 	{
 	    // Your code goes here
-	    string temp=S;
-	    int n=S.length();
-	    for(int i=0;i<n/2;i++)
+	    int left=0, right=S.length()-1;
+	    
+	    while(left<=right)
 	    {
-	        swap(S[i],S[n-i-1]);
+	        if(S[left]!=S[right])
+	        {
+	            return 0;
+	        }
+	        left++;
+	        right--;
 	    }
-	    if(S==temp)
-	    {
-	        return 1;
-	    }
-	    else
-	    {
-	        return 0;
-	    }
+	    return 1;
 	    
 	}
 
 };
+
 
 //{ Driver Code Starts.
 
@@ -54,4 +105,4 @@ int main()
 }
 // } Driver Code Ends
 
-// Question Link -- https://practice.geeksforgeeks.org/problems/palindrome-string0817/1?page=1&difficulty[]=0&status[]=solved&category[]=Strings&sortBy=submissions
+// Question Link -- https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
