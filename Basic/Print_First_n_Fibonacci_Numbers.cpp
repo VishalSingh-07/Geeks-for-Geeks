8bf413fee51ff8b09355ae9513d937b810ec391c
VishalSingh-07 <vishalsinghagr25@gmail.com>
2023-09-17 18:24:16 +0530

    Adding Print First n Fibonacci Numbers

---
+++ Basic/Print_First_n_Fibonacci_Numbers.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+        //code here
+        vector<long long> ans;
+        if(n==1)
+        {
+            return {1};
+        }
+        long long a=1,b=1;
+        ans.push_back(a);
+        ans.push_back(b);
+        for(int i=2;i<n;i++)
+        {
+            long long c=a+b;
+            a=b;
+            b=c;
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+// Question link -- https://practice.geeksforgeeks.org/problems/print-first-n-fibonacci-numbers1002/1
