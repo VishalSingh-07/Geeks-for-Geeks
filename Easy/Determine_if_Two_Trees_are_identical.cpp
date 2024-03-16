4714756af1d52aabc492d458a6abd9d85af6f934
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-03-16 21:15:30 +0530

    Determine if Two Trees are identical

---
+++ Easy/Determine_if_Two_Trees_are_identical.cpp
@@ -0,0 +1,189 @@
+/*
+Determine if Two Trees are Identical
+
+Given two binary trees, the task is to find if both of them are identical or not.
+
+Note: You need to return true or false, the printing is done by the driver code.
+
+Example 1:
+
+Input:
+     1          1
+   /   \      /   \
+  2     3    2     3
+Output: 
+Yes
+Explanation: 
+There are two trees both having 3 nodes and 2 edges, both trees are identical having the root as 1, left child of 1 is 2 and right child of 1 is 3.
+
+
+
+Example 2:
+
+Input:
+    1       1
+  /  \     /  \
+ 2    3   3    2
+Output: 
+No
+Explanation: There are two trees both having 3 nodes and 2 edges, but both trees are not identical.
+
+
+Your task:
+Since this is a functional problem you don't have to worry about input, you just have to complete the function isIdentical() that takes two roots as parameters and returns true or false. The printing is done by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <=Data of a node <= 109
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+// Optimized Approach
+// Time Complexity -> O(n) and Space -> O(h)
+// Where n: number of nodes in the tree and h: height of the tree
+
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        if(r1==nullptr || r2==nullptr){
+            return (r1==r2);
+        }
+        return (r1->data==r2->data) 
+                && isIdentical(r1->left, r2->left) 
+                && isIdentical(r1->right,r2->right);
+    }
+};
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str, str1;
+        getline(cin, str);
+        Node *rootA = buildTree(str);
+        getline(cin, str1);
+        Node *rootB = buildTree(str1);
+        Solution ob;
+        if (ob.isIdentical(rootA, rootB)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/*
+1. Question Link -- https://www.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+
+2. Solution Link -- https://leetcode.com/problems/same-tree/solutions/4783870/best-c-solution-optimized-approach-beats-100-00-of-users-with-c-with-explanation
+
+3. Video Link -- https://youtu.be/BhuvF_-PWS0
+*/
