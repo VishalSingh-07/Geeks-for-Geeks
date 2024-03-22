e7d768da3817c349e63bb4bb2695d565c8cb74dc
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-03-22 17:44:27 +0530

    Maximum Path Sum From any node

---
+++ Medium/Maximum_Path_sum_from_any_node.cpp
@@ -0,0 +1,183 @@
+/*
+Maximum path sum from any node
+
+
+Given a binary tree, the task is to find the maximum path sum. The path may start and end at any node in the tree.
+
+Example 1:
+
+Input:
+     10
+    /  \
+   2   -25
+  / \  /  \
+ 20 1  3  4
+Output: 32
+Explanation: Path in the given tree goes
+like 10 , 2 , 20 which gives the max sum as 32.
+
+
+Example 2:
+
+Input:
+     10
+   /    \
+  2      5
+          \
+          -2
+Output: 17
+Explanation: Path in the given tree goes
+like 2 , 10 , 5 which gives the max sum as 17.
+
+
+Your Task:
+You don't need to take input or print anything. Your task is to complete the function findMaxSum() that takes root as input and returns max sum between any two nodes in the given Binary Tree.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 103
+1 ≤ |Data on node| ≤ 104
+
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User Fuction template for C++
+
+
+// Optimized Approach
+// Time complexity -> O(n) and Space -> O(n)
+// where n: number of nodes in a binary tree
+class Solution {
+    int maxPathDown(Node* node, int &maxi){
+        if(node==nullptr){
+            return 0;
+        }
+        int leftSum=max(0,maxPathDown(node->left,maxi));
+        int rightSum=max(0,maxPathDown(node->right,maxi));
+        
+        maxi=max(maxi,leftSum+rightSum+node->data);
+        
+        return (node->data)+max(leftSum,rightSum);
+    }
+  public:
+    //Function to return maximum path sum from any node in a tree.
+    int findMaxSum(Node* root)
+    {
+        // Your code goes here
+        int maxi=INT_MIN;
+        maxPathDown(root,maxi);
+        
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        cout << ob.findMaxSum(root) << "\n";
+
+    }
+
+
+    return 0;
+}
+// } Driver Code Ends
+
+/*
+1. Question link -- https://www.geeksforgeeks.org/problems/maximum-path-sum-from-any-node/1
+
+2. Solution link -- https://leetcode.com/problems/binary-tree-maximum-path-sum/solutions/4910129/best-c-solution-optimized-approach-with-explanation-striver-solution
+
+3. Video Link -- https://youtu.be/WszrfSwMz58
+*/
