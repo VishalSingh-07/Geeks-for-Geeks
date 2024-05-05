f1080c679140e52445b7406f3d663f2811868be9
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-05-06 00:20:37 +0530

    Print all nodes that don't have sibling

---
+++ Easy/Print_all_nodes_that_don't_have_sibling.cpp
@@ -0,0 +1,193 @@
+/*
+Print all nodes that don't have sibling
+
+Given a Binary Tree of n nodes, find all the nodes that don't have any siblings. You need to return a list of integers containing all the nodes that don't have a sibling in sorted order (Increasing).
+
+Two nodes are said to be siblings if they are present at the same level, and their parents are the same.
+
+Note: The root node can not have a sibling so it cannot be included in our answer.
+
+Example 1:
+
+Input :
+       37
+      /   
+    20
+    /     
+  113 
+
+Output: 
+20 113
+Explanation: 
+Nodes 20 and 113 dont have any siblings.
+
+Example 2:
+
+Input :
+       1
+      / \
+     2   3 
+
+Output: 
+-1
+Explanation: 
+Every node has a sibling.
+
+
+Your Task:  
+- You don't need to read input or print anything. Complete the function noSibling() which takes the root of the tree as input parameter and returns a list of integers containing all the nodes that don't have a sibling in sorted order. If all nodes have a sibling, then the returning list should contain only one element -1.
+
+Expected Time Complexity: O(nlogn)
+Expected Auxiliary Space: O(Height of the tree)
+
+Constraints:
+- 1 ≤ n ≤ 104
+*/
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+vector<int> noSibling(Node *root);
+
+int main()
+{
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        
+        vector<int> res = noSibling(root);
+        for (int i = 0; i < res.size(); ++i)
+        {
+            cout<<res[i]<<" ";
+        }
+        cout<<endl;
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+
+// Optimized Approach
+// Time complexity -> O(nlogn) and Space -> O(h)
+// where n: number of nodes in a binary tree and h: height of the tree
+void traverse(Node* node,vector<int> &ans){
+    if(node==nullptr){
+        return;
+    }
+    if(node->left==nullptr && node->right!=nullptr){
+        ans.push_back(node->right->data);   
+    }
+    if(node->left!=nullptr && node->right==nullptr){
+        ans.push_back(node->left->data);   
+    }
+    traverse(node->left,ans);
+    traverse(node->right,ans);
+}
+
+vector<int> noSibling(Node* node)
+{
+    // code here
+    vector<int> ans;
+    traverse(node,ans);
+    if(ans.empty()){
+        return {-1};
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+    
+}
+
+// Question link -- https://www.geeksforgeeks.org/problems/print-all-nodes-that-dont-have-sibling/1
