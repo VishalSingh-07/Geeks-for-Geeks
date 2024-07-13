1cb2b737c63b832ea9e1467eb479e96121f9afef
VishalSingh-07 <vishalsinghagr25@gmail.com>
2024-07-13 21:20:19 +0530

    Print adjacency list

---
+++ Easy/Print_Adjacency_List.cpp
@@ -0,0 +1,104 @@
+/*
+Print adjacency list
+
+Given an undirected graph with V nodes and E edges, create and return an adjacency list of the graph. 0-based indexing is followed everywhere.
+
+Example 1:
+
+Input:
+V = 5, E = 7
+edges = {(0,1),(0,4),(4,1),(4,3),(1,3),(1,2),(3,2)}
+
+Output: 
+{{1,4}, 
+ {0,2,3,4}, 
+ {1,3},
+ {1,2,4},
+ {0,1,3}}
+Explanation:
+Node 0 is connected to 1 and 4.
+Node 1 is connected to 0,2,3 and 4.
+Node 2 is connected to 1 and 3.
+Node 3 is connected to 1,2 and 4.
+Node 4 is connected to 0,1 and 3.
+
+
+Example 2:
+Input:
+V = 4, E = 3
+edges = {(0,3),(0,2),(2,1)}
+
+Output: 
+{{2,3} 
+ {2}, 
+ {0,1} 
+ {0}}
+Explanation:
+Node 0 is connected to 2 and 3.
+Node 1 is only connected to 2.
+Node 2 is connected to 0 and 1.
+Node 3 is only connected to 0.
+
+
+Your task:
+- You don't need to read input or print anything. Your task is to complete the function printGraph() which takes the integer V denoting the number of vertices and edges as input parameters and returns the list of list denoting the adjacency list.
+
+Expected Time Complexity: O(V + E)
+Expected Auxiliary Space: O(V + E)
+
+Constraints:
+- 1 ≤ V, E ≤ 105
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        // Code here
+        vector<vector<int>> adj(V);
+        for(auto& edge : edges){
+            int u=edge.first;
+            int v=edge.second;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return adj;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int,int>>edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u,v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        for(int i=0;i<V;i++)
+        {
+            sort(adj[i].begin(),adj[i].end());
+            for(auto it:adj[i])
+                cout<<it<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// Question link -- https://www.geeksforgeeks.org/problems/print-adjacency-list-1587115620/1
